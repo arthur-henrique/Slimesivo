9927c0bb4d91b6b11fb925704282e8e40497bbba
arthur-henrique <arthur.henrique.h@outlook.com>
2024-02-26 11:55:25 -0300

    Unity Authentication + Anonymous CheckIn Testing

---
+++ Assets/Scripts/Systems/Authentication/UAuthentication.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using System.Threading.Tasks;
+
+public class UAuthentication : MonoBehaviour
+{
+    private async void Start()
+    {
+        // UnityServices.InitializeAsync() will initialize all services that are subscribed to Core
+        await UnityServices.InitializeAsync();
+        Debug.Log(UnityServices.State);
+
+        SetupEvents();
+        await SignInAnonymouslyAsync();
+    }
+
+    private void SetupEvents()
+    {
+        AuthenticationService.Instance.SignedIn += () => {
+            // Shows how to get a playerID
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+
+            // Shows how to get an access token
+            Debug.Log($"Access Token: {AuthenticationService.Instance.AccessToken}");
+
+        };
+
+        AuthenticationService.Instance.SignInFailed += (err) => {
+            Debug.LogError(err);
+        };
+
+        AuthenticationService.Instance.SignedOut += () => {
+            Debug.Log("Player signed out.");
+        };
+
+        AuthenticationService.Instance.Expired += () =>
+        {
+            Debug.Log("Player session could not be refreshed and expired.");
+        };
+    }
+
+    private async Task SignInAnonymouslyAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("Sign-in Anonymously succeeded!");
+
+            // Shows how to get player ID
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify de player with the proper error message
+            Debug.LogException(ex);
+        } 
+
+    }
+}
