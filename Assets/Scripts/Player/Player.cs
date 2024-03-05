9c5169d9478104d64dbca0dcb9009968f7b34da7
kaykecelli <78507284+kaykecelli@users.noreply.github.com>
2024-03-05 06:13:16 -0300

    fixed update

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -103,8 +103,7 @@ public class Player : MonoBehaviour
             isWallSliding = true;
             StartCoroutine(WallStickTimer());
             canJump = true;
-            isTouchingWall = true;
-            wallJump = true;
+            
             
             
         }
