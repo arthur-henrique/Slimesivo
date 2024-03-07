ee73637a4f5fbfdaa8aff8780aa19949d2309fef
kaykecelli <78507284+kaykecelli@users.noreply.github.com>
2024-03-07 15:09:18 -0300

    Gameplay base

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,11 +8,13 @@ public class Player : MonoBehaviour
     //Jump variables
     [SerializeField] private float jumpForce;
     [SerializeField] private float sideForce;
-    private bool canJump = true;
-    private bool wallJump;
+    [SerializeField] private Transform groundCheck;
+    private int groundLayer;
+    private bool isGrounded = true;
 
     //components
     private Rigidbody rig;
+    private TouchManager touchManager;
 
     //Wall Slide
     private bool isWallSliding;
@@ -22,17 +24,29 @@ public class Player : MonoBehaviour
     private bool isTouchingWall;
     private int wallLayer;
     private Collider[] arrColliders;
+
+    //Wall Jump
+    private bool isWallJumping;
+    private float wallJumpingCounter;
+    private float wallJumpTime = 0.2f;
+    private float wallJumpDurantion = 0.2f;
+    
     private void Start()
     {
         Components();
         wallLayer = LayerMask.NameToLayer("Wall");
+        groundLayer = LayerMask.NameToLayer("Ground");
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         WallStick();
 
     }
+
+
+       
+
 
 
 
@@ -41,48 +55,51 @@ public class Player : MonoBehaviour
     private void Components()
     {
         rig = GetComponent<Rigidbody>();
+        touchManager = GetComponent<TouchManager>();
     }
-    public void JumpRight()
+    public bool IsOnGround()
     {
-        if (canJump)
+        arrColliders = Physics.OverlapSphere(groundCheck.position, 0.2f);
+        foreach (Collider c in arrColliders)
         {
-            if (isTouchingWall)
+            if (c.gameObject.layer == groundLayer)
             {
-                StopAllCoroutines();
-                rig.useGravity = true;
-                rig.velocity = new Vector2(sideForce, jumpForce);
-                canJump = false;
+                return true;
+
             }
-            //rig.AddForce(Vector2.up * jumpForce + Vector2.right * sideForce, ForceMode.Impulse);
+
+        }
+        return false;
+    }
+    public void Jump()
+    {
+        if (touchManager.isFacingRight && IsOnGround())
+        {
             rig.velocity = new Vector2(sideForce, jumpForce);
-            canJump = false;
-
-        }
-
-    }
-    public void JumpLeft()
-    {
-        if (canJump)
-        {
-            if (isTouchingWall)
-            {
-                StopAllCoroutines();
-                rig.useGravity = true;
-                rig.velocity = new Vector2(-sideForce, jumpForce);
-                canJump = false;
-                wallJump = true;
-            }
             
-            rig.velocity = new Vector2(-sideForce, jumpForce);
-            canJump = false;
+
         }
+        else if(IsOnGround() && !touchManager.isFacingRight)
+        {
+            rig.velocity = new Vector2(-sideForce, jumpForce);
+        }
+
+        if(!IsOnGround() && IsWalled())
+        {
+            WallJump();
+        }
+
+
+
+
     }
+   
     #endregion
 
     #region WallSlide
     private bool IsWalled()
     {
-        arrColliders = Physics.OverlapSphere(wallCheck.position, 0.2f);
+        arrColliders = Physics.OverlapSphere(wallCheck.position, 0.6f);
         foreach (Collider c in arrColliders)
         {
             if (c.gameObject.layer == wallLayer)
@@ -98,21 +115,23 @@ public class Player : MonoBehaviour
 
     private void WallStick()
     {
-        if (IsWalled() && !canJump)
+        if (IsWalled() && !IsOnGround() && !isWallJumping)
         {
             isWallSliding = true;
+            
             StartCoroutine(WallStickTimer());
-            canJump = true;
             
             
             
+            
+        }
+        else
+        {
+            isWallSliding = false;
         }
        
 
-        if (isWallSliding)
-        {
-            rig.velocity = new Vector2(rig.velocity.x, Mathf.Clamp(rig.velocity.y, -wallSlidingSpeed, float.MaxValue));
-        }
+       
     }
     IEnumerator WallStickTimer()
     {
@@ -120,20 +139,54 @@ public class Player : MonoBehaviour
         rig.useGravity = false;
         while (timer < wallStickTime)
         {
-            if (!wallJump)
-            {
+            
                 rig.velocity = new Vector2(0, 0);
                 timer += Time.deltaTime;
                 yield return null;
-            }
+            
             
            
         }
         rig.useGravity = true;
-        isWallSliding = true;
+        
     }
 
+    private void WallJump()
+    {
+        StopAllCoroutines();
+        rig.useGravity = true;
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingCounter = wallJumpTime;
+            CancelInvoke(nameof(StopWallJump));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
 
+        if (wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            wallJumpingCounter = 0;
+            if (touchManager.isFacingRight)
+            {
+                rig.velocity = new Vector2(sideForce, jumpForce);
+
+
+            }
+            else if (!touchManager.isFacingRight)
+            {
+                rig.velocity = new Vector2(-sideForce, jumpForce);
+            }
+            Invoke(nameof(StopWallJump), wallJumpDurantion);
+        }
+    }
+    private void StopWallJump()
+    {
+        isWallJumping = false;
+    }
        
     #endregion
 }
--- Assets/Scripts/Player/TouchManager.cs
+++ Assets/Scripts/Player/TouchManager.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class TouchManager : MonoBehaviour
 {
-    
+    //Input
     private PlayerInput playerInput;
     private float screenSide;
     private InputAction touchPositionAction;
     private InputAction _touchPressAction;
+    
     private Player playerScript;
+
+    //Direction
+    private float _rightCounter;
+    private float _leftCounter;
+
+    private bool _isFacingRight;
+
+    public bool isFacingRight
+    {
+        get { return _isFacingRight; }
+        set { _isFacingRight = value; }
+    }
+
+   
 
     public InputAction touchPressAction
     {
@@ -32,27 +47,32 @@ public class TouchManager : MonoBehaviour
     private void OnEnable()
     {
         _touchPressAction.performed += TouchPress;
+        
     }
     private void OnDisable()
     {
         _touchPressAction.performed -= TouchPress;
     }
 
+    
     private void TouchPress(InputAction.CallbackContext context)
     {
        //Pega o valor do pixel onde o player clica e divide pelo valor total de pixeis da tela
         Vector2 value = touchPositionAction.ReadValue<Vector2>();
         screenSide = value.x / Camera.main.pixelWidth;
         
+        
         //Dai checa pra ver se foi esquerda ou direita, maior q 0.5 direita menor esquerda
         if(screenSide > 0.5)
         {
-            playerScript.JumpRight();
+            _isFacingRight = true;
+            playerScript.Jump();
             Debug.Log("Direita");
         }
         else
         {
-            playerScript.JumpLeft();
+            _isFacingRight = false;
+            playerScript.Jump();
             Debug.Log("Esquerda");
         }
         
