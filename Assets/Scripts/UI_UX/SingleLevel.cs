7739c4eb64ed892d391fa92f7bf2ddbe8f68c371
Anna Beatriz Nakai Ricchetti <104537504+AmazingProgramadora@users.noreply.github.com>
2024-03-25 10:52:40 -0300

    UI_UX progressao na criacao do mapa e da informacao das estrelas nas fases

--- Assets/Scripts/UI_UX/SingleLevel.cs
+++ Assets/Scripts/UI_UX/SingleLevel.cs
@@ -5,13 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SingleLevel : MonoBehaviour
 {
+    private int currentStarNumber = 0;
+    private int currentLevelIndex;
+
+    private void Start()
+    {
+        currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+        print(currentLevelIndex);
+    }
     public void BackButton()
     {
-        SceneManager.LoadScene("UI_Test_Map");
+        //SceneManager.LoadScene("UI_Test_Map");
+        PlayerPrefs.DeleteKey("Lv" + currentLevelIndex);
     }
 
-    public void PressStarsButton(int _StarsNumber)
+    public void PressStarsButton(int _starsNumber)
     {
+        currentStarNumber = _starsNumber;
+        if (currentStarNumber > PlayerPrefs.GetInt("Lv" + currentLevelIndex)) //vai salvar a pontuacao (estrelas) somente se for maior que a anterior
+        {
+            PlayerPrefs.SetInt("Lv" + currentLevelIndex, _starsNumber);
+        }
+        print(PlayerPrefs.GetInt("Lv" + currentLevelIndex, _starsNumber));
 
     }
 }
