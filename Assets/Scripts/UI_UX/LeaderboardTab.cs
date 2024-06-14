c35f1a41208c9ebcdb44746118b9eeaab429b70d
Anna Beatriz Nakai Ricchetti <anninha0307trabalhos@gmail.com>
2024-06-14 01:10:24 -0300

    Script do Leaderboard Toggle Button ajustado

--- Assets/Scripts/UI_UX/LeaderboardTab.cs
+++ Assets/Scripts/UI_UX/LeaderboardTab.cs
@@ -2,28 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LeaderboardTab : MonoBehaviour
 {
-    [SerializeField] GameObject[] Tabs;
-    [SerializeField] Image[] TabButtons;
-    [SerializeField] Sprite InactiveTabBG, ActiveTabBG;
-    [SerializeField] Vector2 InactiveTabButtonSize, ActiveTabButtonSize;
+    [SerializeField] private GameObject[] tabs;
+    [SerializeField] private Image[] tabButtons;
+    [SerializeField] private Sprite inactiveTabBG, activeTabBG;
+    [SerializeField] private TMP_Text friendsText, globalText;
+    private bool isTheFirstButtonActive = true;
+    [SerializeField] private int inactiveTextColorRed, inactiveTextColorGreen, inactiveTextColorBlue;
+    [SerializeField] private int activeTextColorRed, activeTextColorGreen, activeTextColorBlue;
+    [SerializeField] private Vector2 inactiveTabButtonSize, activeTabButtonSize;
 
     public void SwitchToTab (int TabID)
     {
-        foreach (GameObject go in Tabs)
+
+        foreach (GameObject go in tabs) //esconde ou mostra o que tem dentro de cada tab
         {
             go.SetActive (false);
         }
-        Tabs[TabID].SetActive (true);
+        tabs[TabID].SetActive (true);
 
-        foreach (Image im in TabButtons)
+        foreach (Image im in tabButtons)
         {
-            im.sprite = InactiveTabBG;
-            im.rectTransform.sizeDelta = InactiveTabButtonSize;
+            im.sprite = inactiveTabBG;
+            im.rectTransform.sizeDelta = inactiveTabButtonSize;
         }
-        TabButtons[TabID].sprite = ActiveTabBG;
-        TabButtons[TabID].rectTransform.sizeDelta = ActiveTabButtonSize;
+        tabButtons[TabID].sprite = activeTabBG;
+        tabButtons[TabID].rectTransform.sizeDelta = activeTabButtonSize;
+
+
+        if (isTheFirstButtonActive == true) //minha funcao de trocar a cor dos textos
+        {
+            isTheFirstButtonActive = false;
+            friendsText.color = new Color(activeTextColorRed / 255f, activeTextColorGreen / 255f, activeTextColorBlue / 255f);
+            globalText.color = new Color(inactiveTextColorRed / 255f, inactiveTextColorGreen / 255f, inactiveTextColorBlue / 255f);
+        }
+        else
+        {
+            isTheFirstButtonActive = true;
+            globalText.color = new Color(activeTextColorRed / 255f, activeTextColorGreen / 255f, activeTextColorBlue / 255f);
+            friendsText.color = new Color(inactiveTextColorRed / 255f, inactiveTextColorGreen / 255f, inactiveTextColorBlue / 255f);
+        }
+    }
+
+    void Update()
+    {
+        print(isTheFirstButtonActive);
     }
 }
